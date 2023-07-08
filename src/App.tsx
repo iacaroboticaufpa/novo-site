ed197f5e71a6beaa76024ec1640daa923e3a473f
Ana Paula Fonseca <ana.eng.dev@gmail.com>
2023-07-08 20:00:30 -0300

    primeiro deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import {Home} from './pages/home/home';
+import {Projects} from './pages/projetos/projetos';
+import {Sobre} from './pages/sobre/sobre';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<Sobre />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/projetos/projetos.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Projects() {
+  return <h1>Página de Projetos</h1>;
+}
+
+export {Projects};
