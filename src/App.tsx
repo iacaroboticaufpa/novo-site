bc42e672ad8fa8173598a45c2b433989032be969
Ana Paula Fonseca <ana.eng.dev@gmail.com>
2023-07-08 20:10:10 -0300

    ajsutando roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import {Home} from './pages/home/home';
 import {Projects} from './pages/projetos/projetos';
